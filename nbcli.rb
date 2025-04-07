7807b628cef2e57ce6e1ed2882313659d8cddb0c
MATTHIEU BOEYKENS <19000026@EHFR-M-K9R46Y5611.hq.corp.leroymerlin.com>
2025-04-07 20:49:42 +0200

    release 1.1.0

--- nbcli.rb
+++ nbcli.rb
@@ -1,9 +1,9 @@
 class Nbcli < Formula
   desc "Ton CLI Kotlin/Native"
   homepage "https://github.com/MatthieuBoeykens/homebrew-nbcli"
-  url "https://github.com/MatthieuBoeykens/homebrew-nbcli/releases/download/v1.0.0/nbCli.tar.gz"
-  sha256 "f34425e9aabd0cf4dd1e0c8cdaad332c3442353de6b60997b21a668db1266dfe"
-  version "1.0.0"
+  url "https://github.com/MatthieuBoeykens/homebrew-nbcli/releases/download/v1.1.0/nbCli.tar.gz"
+  sha256 "5188c3ae7cc7dae4ae4723d41e91cd300f7602e158ec1db7327f52f66bb4e12a"
+  version "1.1.0"
 
   def install
     bin.install "nbCli"
